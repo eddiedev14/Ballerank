e7f5af3a96efe7518356e81d90ea3b9d56da9ad5
Eddie Santiago <eddiesantiago122007@gmail.com>
2025-11-01 19:03:46 -0500

    ♻️ refactor: Change map to object to simplify complexity calculation

--- js/util.js
+++ js/util.js
@@ -5,9 +5,9 @@ export default function resolverConcursoTriples(concurso){
     const lanzamientos = info[1].split("*") // -> [lanzamiento1, lanzamiento2, lanzamiento3]
 
     // * Crear un objeto para mapear los lanzamientos, donde la key es el nombre del jugador y el valor un array con sus lanzamientos
-    const lanzamientosMapper = new Map();
+    const lanzamientosMapper = {};
     jugadores.forEach(jugador => {
-        lanzamientosMapper.set(jugador, []);
+        lanzamientosMapper[jugador] = [];
     });
 
     // * Añadir lanzamientos en el objeto lanzamientosMapper
@@ -20,7 +20,7 @@ export default function resolverConcursoTriples(concurso){
         const tiros = tirosStr.split(" ").map(x => parseInt(x)) // -> [tiro1, tiro2, tiro3]
 
         // Añadir los tiros al jugador correspondiente
-        lanzamientosMapper.get(jugador).push(tiros)
+        lanzamientosMapper[jugador].push(tiros)
     })
 
     console.log(lanzamientosMapper)
